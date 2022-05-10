07cc4a9e2c3703adcee77717cbddee3d350230ab
kajela <titoyut56@gmail.com>
2022-05-10 06:51:58 -0400

    Initial commit

---
+++ src/attribute.java
@@ -0,0 +1,10 @@
+public class attribute {
+    String fname ="tito";
+    String lname ="muchoki";
+    int age = 22;
+    public static void main(String[] args) {
+        attribute name = new attribute();
+        System.out.println("NAME: "  +name.fname  +name.lname);
+        System.out.println("age: "+name.age);
+    }
+}
